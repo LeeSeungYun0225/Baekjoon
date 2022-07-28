d72ff942a66bd8dbfe7ac8a0dbd0e1083a7cfc00
Seung Yun LEE <soul3914@naver.com>
2022-07-29 03:35:24 +0900

    1000

---
+++ src/B1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class B1000 {
+	public static void main(String[] args) {
+		int a,b;
+		
+		Scanner scanner = new Scanner(System.in);
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		
+		System.out.print(a+b);
+		
+	}
+}
