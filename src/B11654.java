9357499caa71536fdecfa255b50d0d1c6c2ca5be
Seung Yun LEE <soul3914@naver.com>
2022-08-02 19:01:26 +0900

    11654

---
+++ src/B11654.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class B11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		char in = scan.next().charAt(0);
+		
+		
+		
+		
+		
+		System.out.print((int)in);
+	}
+
+}
